7869c2e54a0449bbcf39c55ad30b900a607cffcb
Brad Seefeld <bradley.seefeld@gmail.com>
2011-05-06 23:04:03 -0700

    Overloaded some methods

--- src/main/java/com/bradley/gwt/user/client/ui/Mask.java
+++ src/main/java/com/bradley/gwt/user/client/ui/Mask.java
@@ -1,20 +1,20 @@
 package com.bradley.gwt.user.client.ui;
 
 import com.google.gwt.user.client.Element;
-import com.google.gwt.user.client.ui.UIObject;
+import com.google.gwt.user.client.ui.IsWidget;
 
 public class Mask {
 	
-	public static void mask(UIObject element) {
+	public static void mask(IsWidget element) {
 		mask(element, null);
 	}
 	
-	public static void mask(UIObject element, String message) {
-		_mask(element.getElement(), message);
+	public static void mask(IsWidget element, String message) {
+		_mask(element.asWidget().getElement(), message);
 	}
 	
-	public static void unmask(UIObject element) {
-		_unmask(element.getElement());
+	public static void unmask(IsWidget element) {
+		_unmask(element.asWidget().getElement());
 	}
 	
 	public static native void _mask(Element element, String message) /*-{
